4b3d56d8c8ca3dddd47a8194f3d08a1e7d8c3119
not-my-laptop <clack12@icloud.com>
2024-12-03 18:49:28 +0900

    아니 분명 코밋이랑 푸시 하고왔는데 ㅅㅂ 왜날라감 ㅜㅜㅜㅜㅜㅜ git 존나어렵네

--- client.ts
+++ client.ts
@@ -1,16 +1,24 @@
+import type * as types from "./types";
+
 export class LaftelClient {
   private headers: Record<string, string>;
   constructor(token?: string) {
     this.headers = {
-      'User-Agent': 'Mozilla/5.0 (compatible; Googlebot/0; +https://googlebot.com)',
-      'Referrer-Policy': 'no-referrer',
-      'DNT': "1",
+      "User-Agent":
+        "Mozilla/5.0 (compatible; Googlebot/0; +https://googlebot.com)",
+      "Referrer-Policy": "no-referrer",
+      DNT: "1",
     };
-    if (token) this.headers["Authorization"] = "Token " + token; // actually required
+    if (token)
+      this.headers["Authorization"] = "Token " + token; // actually required
     else console.warn("LaftelClient created without a Token.");
     // TODO: check if the token valid. (let's add it to option/parameter)
   }
-  async searchAnime(query: string, size: number = 24, offset: number = 0): Promise<ItemV3[]> {
+  async searchAnime(
+    query: string,
+    size: number = 24,
+    offset: number = 0,
+  ): Promise<ItemV3[]> {
     const url = new URL(`https://api.laftel.net/api/search/v3/keyword/`);
     url.searchParams.set("keyword", query);
     url.searchParams.set("size", size.toString());
@@ -27,9 +35,16 @@ export class LaftelClient {
     const data = await res.json();
     return data["results"];
   }
+  /** 에피소드 목록 쿼리
+   * @param id 애니메이션 ID
+   * @returns 애니메이션 ID에 대한 에피소드 목록
+   */
   async getEpisodes(id: number): Promise<object[]> {
-    const res = await fetch(`https://api.laftel.net/api/${id}/`, {headers:this.headers}); // FIXME: f12
+    const res = await fetch(
+      `https://api.laftel.net/api/episodes/v2/list/?item_id=${id}&sort=oldest&limit=1000&show_playback_offset=false&offset=0`,
+      { headers: this.headers },
+    );
     const data = await res.json();
-    return data;
+    return data["results"];
   }
 }
