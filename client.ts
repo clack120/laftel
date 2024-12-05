619035c15d39a38972e99e23d7764911e8b5aeb7
Clack12 <115343194+clack120@users.noreply.github.com>
2024-12-05 13:54:53 +0900

    Update client.ts
    난바보야

--- client.ts
+++ client.ts
@@ -1,42 +1,109 @@
-import { LaftelClient } from "./client.ts";
 import type * as types from "./types.d.ts";
-const env = {
-  get: globalThis.Deno
-    ? (key: string) => globalThis.Deno.env.get(key) // 예외처리하기
-    : (key: string) => globalThis.process.env[key],
-};
+const BASE_URL = "https://api.laftel.net/api";
 
-const client = new LaftelClient(env.get("LAFTEL_TOKEN"));
+export class LaftelClient {
+  private headers: Record<string, string> = {};
+  constructor(token?: string) {
+    this.headers = {
+      "User-Agent":
+        "Mozilla/5.0 (compatible; Linux x86_64; Google  bot/0; +https://googlebot.com)",
+      Accept: "application/json, */*",
+      "Accept-Encoding": "gzip, deflate, br",
+      Origin: "https://laftel.net",
+      Referer: "https://laftel.net",
+      "Referrer-Policy": "no-referrer",
+      "Sec-GPG": "1",
+      DNT: "1",
+      //"Sec-Fetch-Dest": "empty",
+      //"Sec-Fetch-Mode": "cors",
+      //"Sec-Fetch-Site": "same-site",
+      //TE: "trailers",
+    };
+    if (token)
+      this.headers["Authorization"] = "Token " + token; // actually required to watch or download anime
+    else console.warn("LaftelClient created without a Token.");
+    // TODO: check if the token valid. (let's add it to option/parameter)
+  }
 
-let res: types.EpisodeV2[] | types.ItemV3[] = await client.searchAnime(
-  prompt("search anime:") || "",
-  globalThis.process?.stdout?.rows || 9,
-);
-res.forEach((anime, index) => {
-  console.log(index + 1, anime.name);
-});
-let idx = parseInt(prompt("select anime:") || "0") - 1; // TODO: 예외처리
-res = await client.getEpisodes(res[idx].id);
+  private async fetchData<T>(url: string): Promise<T> {
+    const res = await fetch(url, { headers: this.headers });
+    if (!res.ok) throw new Error(`Failed to fetch: ${url}`);
+    const data = await res.json();
+    return data.results;
+  }
 
-res.forEach((episode, index) => {
-  console.log(index + 1, episode.subject);
-});
-idx = parseInt(prompt("select anime:") || "0") - 1; // TODO: 예외처리
+  setHeader(key: string, value: string): Boolean {
+    if (key == "Authorization") return false;
+    this.headers[key] = value;
+    return true;
+  }
+  getHeader(key?: string): string | Record<string, string> {
+    if (key) return this.headers[key];
+    return this.headers;
+  }
+  setToken(token: string | null) {
+    if (token) this.headers["Authorization"] = "Token " + token;
+    else delete this.headers["Authorization"];
+  }
 
-const episode = res[idx];
-const downloadURL = await client.getMPDPath(episode);
-console.log(downloadURL);
-// https://mediacloud.laftel.net/2023/05/68373/v15/video/dash/stream.mpd
-const process = Deno.run({
-  cmd: [
-    "bin/yt-dlp",
-    "--allow-unplayable-formats",
-    "-o",
-    `${episode.id}.%(ext)s`,
-    downloadURL,
-  ],
-  stdout: "piped",
-  stderr: "piped",
-});
-const status = await process.status();
-process.close();
+  async searchAnime(
+    query: string,
+    size = 24,
+    offset = 0,
+  ): Promise<types.ItemV3[]> {
+    const url = new URL(`${BASE_URL}/search/v3/keyword/`);
+    url.searchParams.set("keyword", query);
+    url.searchParams.set("size", size.toString());
+    url.searchParams.set("offset", offset.toString());
+    return await this.fetchData(url.toString());
+  }
+
+  async getAnimeInfo(id: number): Promise<types.ItemV3> {
+    return await this.fetchData(`${BASE_URL}/${id}/`);
+  }
+
+  async getEpisodes(id: number): Promise<types.EpisodeV2[]> {
+    return await this.fetchData(
+      `${BASE_URL}/episodes/v2/list/?item_id=${id}&sort=oldest&limit=1000&show_playback_offset=false&offset=0`,
+    );
+  }
+
+  async getEpisode(id: number): Promise<types.EpisodeV2> {
+    return await this.fetchData(`${BASE_URL}/episodes/v2/${id}/`);
+  }
+
+  async getMPDPath(episode: number | types.EpisodeV2): Promise<string> {
+    if (typeof episode === "number") episode = await this.getEpisode(episode);
+
+    const { thumbnail_path, published_datetime, id } = episode;
+    if (thumbnail_path.includes("/v15/Thumbnail.")) {
+      const path = thumbnail_path.substring(
+        thumbnail_path.indexOf("s/") + 2,
+        thumbnail_path.indexOf("/v"),
+      );
+      return `https://mediacloud.laftel.net/${path}/v15/video/dash/stream.mpd`;
+    } else {
+      const date = new Date(published_datetime);
+      const year = date.getFullYear();
+      const month = String(date.getMonth() + 1).padStart(2, "0");
+      return `https://mediacloud.laftel.net/${year}/${month}/${id}/v15/video/dash/stream.mpd`;
+    }
+  }
+
+  async getStream(episode: number | types.EpisodeV2) {
+    if (typeof episode === "number") episode = await this.getEpisode(episode);
+
+    const { id } = episode;
+    const stream: types.StreamingInfoV2 = await this.fetchData(
+      `https://api.laftel.net/api/episodes/v2/${id}/video/?device=Web`,
+    );
+    return stream;
+  }
+
+  async downloadEpisode(id: number) {
+    return "/";
+  }
+}
+
+// https://api.laftel.net/api/profiles/v1/my_profile/
+// https://api.laftel.net/api/profiles/v1/${위의 res의 id}/token/
